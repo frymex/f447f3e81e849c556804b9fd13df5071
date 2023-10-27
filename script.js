bdfa8168ece9065e88cff314506b70f8a036c9da
frymex <t_frymex@protonmail.com>
2023-10-27 18:00:59 +0300

    Update v2

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
+const tetherIcon = "<svg  class='svg-title' xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 2000 2000\" width=\"20\" height=\"20\"><path d=\"M1000 0c552.26 0 1000 447.74 1000 1000s-447.76 1000-1000 1000S0 1552.38 0 1000 447.68 0 1000 0\" fill=\"#53ae94\"/><path d=\"M1123.42 866.76V718h340.18V491.34H537.28V718H877.5v148.64C601 879.34 393.1 934.1 393.1 999.7s208 120.36 484.4 133.14v476.5h246V1132.8c276-12.74 483.48-67.46 483.48-133s-207.48-120.26-483.48-133m0 225.64v-.12c-6.94.44-42.6 2.58-122 2.58-63.48 0-108.14-1.8-123.88-2.62v.2C633.34 1081.66 451 1039.12 451 988.22S633.36 894.84 877.62 884v166.1c16 1.1 61.76 3.8 124.92 3.8 75.86 0 114-3.16 121-3.8V884c243.8 10.86 425.72 53.44 425.72 104.16s-182 93.32-425.72 104.18\" fill=\"#fff\"/></svg>"
+const btcIcon = "<svg  class='svg-title' xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0.004 0 63.993 64\"><path d=\"M63.04 39.741c-4.274 17.143-21.638 27.575-38.783 23.301C7.12 58.768-3.313 41.404.962 24.262 5.234 7.117 22.597-3.317 39.737.957c17.144 4.274 27.576 21.64 23.302 38.784z\" fill=\"#f7931a\"/><path d=\"M46.11 27.441c.636-4.258-2.606-6.547-7.039-8.074l1.438-5.768-3.512-.875-1.4 5.616c-.922-.23-1.87-.447-2.812-.662l1.41-5.653-3.509-.875-1.439 5.766c-.764-.174-1.514-.346-2.242-.527l.004-.018-4.842-1.209-.934 3.75s2.605.597 2.55.634c1.422.355 1.68 1.296 1.636 2.042l-1.638 6.571c.098.025.225.061.365.117l-.37-.092-2.297 9.205c-.174.432-.615 1.08-1.609.834.035.051-2.552-.637-2.552-.637l-1.743 4.02 4.57 1.139c.85.213 1.683.436 2.502.646l-1.453 5.835 3.507.875 1.44-5.772c.957.26 1.887.5 2.797.726L27.504 50.8l3.511.875 1.453-5.823c5.987 1.133 10.49.676 12.383-4.738 1.527-4.36-.075-6.875-3.225-8.516 2.294-.531 4.022-2.04 4.483-5.157zM38.087 38.69c-1.086 4.36-8.426 2.004-10.807 1.412l1.928-7.729c2.38.594 10.011 1.77 8.88 6.317zm1.085-11.312c-.99 3.966-7.1 1.951-9.083 1.457l1.748-7.01c1.983.494 8.367 1.416 7.335 5.553z\" fill=\"#fff\"/></svg>"
+const tonIcon = "<svg  class='svg-title' width=\"20\" height=\"20\" viewBox=\"0 0 56 56\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+    "<path d=\"M28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 0 28 0C12.536 0 0 12.536 0 28C0 43.464 12.536 56 28 56Z\" fill=\"#0098EA\"/>\n" +
+    "<path d=\"M37.5603 15.6277H18.4386C14.9228 15.6277 12.6944 19.4202 14.4632 22.4861L26.2644 42.9409C27.0345 44.2765 28.9644 44.2765 29.7345 42.9409L41.5381 22.4861C43.3045 19.4251 41.0761 15.6277 37.5627 15.6277H37.5603ZM26.2548 36.8068L23.6847 31.8327L17.4833 20.7414C17.0742 20.0315 17.5795 19.1218 18.4362 19.1218H26.2524V36.8092L26.2548 36.8068ZM38.5108 20.739L32.3118 31.8351L29.7417 36.8068V19.1194H37.5579C38.4146 19.1194 38.9199 20.0291 38.5108 20.739Z\" fill=\"white\"/>\n" +
+    "</svg>"
+const bnbIcon = "<svg   class='svg-title' xmlns=\"http://www.w3.org/2000/svg\"  width=\"20\" height=\"20\"  xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n" +
+    "\t viewBox=\"0 0 2496 2496\" style=\"enable-background:new 0 0 2496 2496;\" xml:space=\"preserve\">\n" +
+    "<g>\n" +
+    "\t<path style=\"fill-rule:evenodd;clip-rule:evenodd;fill:#F0B90B;\" d=\"M1248,0c689.3,0,1248,558.7,1248,1248s-558.7,1248-1248,1248\n" +
+    "\t\tS0,1937.3,0,1248S558.7,0,1248,0L1248,0z\"/>\n" +
+    "\t<path style=\"fill:#FFFFFF;\" d=\"M685.9,1248l0.9,330l280.4,165v193.2l-444.5-260.7v-524L685.9,1248L685.9,1248z M685.9,918v192.3\n" +
+    "\t\tl-163.3-96.6V821.4l163.3-96.6l164.1,96.6L685.9,918L685.9,918z M1084.3,821.4l163.3-96.6l164.1,96.6L1247.6,918L1084.3,821.4\n" +
+    "\t\tL1084.3,821.4z\"/>\n" +
+    "\t<path style=\"fill:#FFFFFF;\" d=\"M803.9,1509.6v-193.2l163.3,96.6v192.3L803.9,1509.6L803.9,1509.6z M1084.3,1812.2l163.3,96.6\n" +
+    "\t\tl164.1-96.6v192.3l-164.1,96.6l-163.3-96.6V1812.2L1084.3,1812.2z M1645.9,821.4l163.3-96.6l164.1,96.6v192.3l-164.1,96.6V918\n" +
+    "\t\tL1645.9,821.4L1645.9,821.4L1645.9,821.4z M1809.2,1578l0.9-330l163.3-96.6v524l-444.5,260.7v-193.2L1809.2,1578L1809.2,1578\n" +
+    "\t\tL1809.2,1578z\"/>\n" +
+    "\t<polygon style=\"fill:#FFFFFF;\" points=\"1692.1,1509.6 1528.8,1605.3 1528.8,1413 1692.1,1316.4 1692.1,1509.6 \t\"/>\n" +
+    "\t<path style=\"fill:#FFFFFF;\" d=\"M1692.1,986.4l0.9,193.2l-281.2,165v330.8l-163.3,95.7l-163.3-95.7v-330.8l-281.2-165V986.4\n" +
+    "\t\tL968,889.8l279.5,165.8l281.2-165.8l164.1,96.6H1692.1L1692.1,986.4z M803.9,656.5l443.7-261.6l444.5,261.6l-163.3,96.6\n" +
+    "\t\tl-281.2-165.8L967.2,753.1L803.9,656.5L803.9,656.5z\"/>\n" +
+    "</g>\n" +
+    "</svg>"
+const ethLogo = "<svg class='svg-title' width=\"20\" height=\"20\" xmlns=\"http://www.w3.org/2000/svg\"  xml:space=\"preserve\" width=\"100%\" height=\"100%\" version=\"1.1\" shape-rendering=\"geometricPrecision\" text-rendering=\"geometricPrecision\" image-rendering=\"optimizeQuality\" fill-rule=\"evenodd\" clip-rule=\"evenodd\"\n" +
+    "viewBox=\"0 0 784.37 1277.39\"\n" +
+    " xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n" +
+    " xmlns:xodm=\"http://www.corel.com/coreldraw/odm/2003\">\n" +
+    " <g id=\"Layer_x0020_1\">\n" +
+    "  <metadata id=\"CorelCorpID_0Corel-Layer\"/>\n" +
+    "  <g id=\"_1421394342400\">\n" +
+    "   <g>\n" +
+    "    <polygon fill=\"#343434\" fill-rule=\"nonzero\" points=\"392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54 \"/>\n" +
+    "    <polygon fill=\"#8C8C8C\" fill-rule=\"nonzero\" points=\"392.07,0 -0,650.54 392.07,882.29 392.07,472.33 \"/>\n" +
+    "    <polygon fill=\"#3C3C3B\" fill-rule=\"nonzero\" points=\"392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89 \"/>\n" +
+    "    <polygon fill=\"#8C8C8C\" fill-rule=\"nonzero\" points=\"392.07,1277.38 392.07,956.52 -0,724.89 \"/>\n" +
+    "    <polygon fill=\"#141414\" fill-rule=\"nonzero\" points=\"392.07,882.29 784.13,650.54 392.07,472.33 \"/>\n" +
+    "    <polygon fill=\"#393939\" fill-rule=\"nonzero\" points=\"0,650.54 392.07,882.29 392.07,472.33 \"/>\n" +
+    "   </g>\n" +
+    "  </g>\n" +
+    " </g>\n" +
+    "</svg>\n"
+const lztLogo = "\n" +
+    "<svg  class='svg-title' width=\"20\" height=\"20\" viewBox=\"0 0 64 64\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+    "<g clip-path=\"url(#clip0_175_69)\">\n" +
+    "<path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M17.5371 51.8461C22.9532 56.246 29.21 58.7535 35.8828 58.7535C45.8058 58.7535 54.8137 53.1919 61.4984 44.1334C58.6759 40.3098 55.4406 37.1071 51.8933 34.6858L46.1979 37.529C47.422 39.4334 48.135 41.7002 48.135 44.1334C48.135 50.8924 42.6506 56.3708 35.8828 56.3708C30.6242 56.3708 26.141 53.0612 24.403 48.4146L17.5371 51.8431V51.8461ZM31.667 44.79C31.8036 45.6783 32.2196 46.5043 32.8643 47.149C33.6635 47.9482 34.7508 48.3968 35.8828 48.3938C38.2387 48.3938 40.1491 46.4864 40.1491 44.1334C40.1491 42.9777 39.6886 41.929 38.9399 41.1595L31.6699 44.79H31.667Z\" fill=\"#2BAD72\"/>\n" +
+    "<path d=\"M19.0882 44.038L60.6014 24.082L51.4776 5.10352L46.7656 21.8746L41.103 10.0918L36.3881 26.8628L30.7254 15.08L26.0105 31.8511L20.3479 20.0682L15.633 36.8393L9.97035 25.0565L2.36768 52.0774L12.7452 47.0891L19.0882 44.0409V44.038Z\" fill=\"#2BAD72\"/>\n" +
+    "</g>\n" +
+    "<defs>\n" +
+    "<clipPath id=\"clip0_175_69\">\n" +
+    "<rect width=\"59.1279\" height=\"53.6495\" fill=\"white\" transform=\"translate(2.36719 5.10376)\"/>\n" +
+    "</clipPath>\n" +
+    "</defs>\n" +
+    "</svg>"
+
 const paymentMethods = {
         "any": {
             "methods": [
-                {"TRC20 (Tether)": "TYZummwRaq2KFJcWjW8rw3eiZsFLjaKWYw"},
+                {"Tether (TRC20)": "TYZummwRaq2KFJcWjW8rw3eiZsFLjaKWYw"},
                 {"TON (The Open Network)": "UQB-yl4o65zi9MdOdCEQ83PF6nZIC0ZfD-roqwzKfBaY_Q-s"},
                 {"BTC (Bitcoin)": "bc1qelx9zythl5xmqzs4jgftyagnnmhqxtt2x9f9x5"},
                 {"BNB (Binance)": "bnb1alfrfft4jh3rmg5dwmjgccdyqydxhtm3fc8cxn"},
-                {"ETH (Ethereum)": "0x270fc6EE9a7f0a491486e784C9be406C36A4F710"}
+                {"ETH (Ethereum)": "0x270fc6EE9a7f0a491486e784C9be406C36A4F710"},
+                {"Lolz (<a href=https://zelenka.guru>zelenka.guru</a>)": "https://zelenka.guru/payment/balance-transfer?user_id=3958441"}
             ],
-            "title": "Support to cazqev's projects",
-            "document__title": "Cazqev's projects support"
+            "icons": {
+                "Tether (TRC20)": tetherIcon,
+                "BTC (Bitcoin)": btcIcon,
+                "TON (The Open Network)": tonIcon,
+                "BNB (Binance)": bnbIcon,
+                "ETH (Ethereum)": ethLogo,
+                "Lolz (<a href=https://zelenka.guru>zelenka.guru</a>)": lztLogo
+            },
+            "title": "Support cazqev's projects",
+            "document__title": "support cazqev's projects"
         }
     }
 ;
@@ -33,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             const paymentTitle = document.createElement('div');
             paymentTitle.classList.add('payment-title');
-            paymentTitle.textContent = Object.keys(wallet)[0] + ":";
+            paymentTitle.textContent = paymentMethods[host]["icons"][Object.keys(wallet)[0]] + Object.keys(wallet)[0] + ":";
             paymentMethod.appendChild(paymentTitle);
 
             const walletId = document.createElement('div');
@@ -88,7 +152,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
             const paymentTitle = document.createElement('div');
             paymentTitle.classList.add('payment-title');
-            paymentTitle.textContent = Object.keys(wallet)[0] + ":";
+            if (paymentMethods[host]["icons"][Object.keys(wallet)[0]]) {
+               paymentTitle.innerHTML = paymentMethods[host]["icons"][Object.keys(wallet)[0]] + " " + Object.keys(wallet)[0] + ":";
+            } else {
+                paymentTitle.innerHTML = Object.keys(wallet)[0] + ":";
+
+            }
             paymentMethod.appendChild(paymentTitle);
 
             const walletId = document.createElement('div');
